b5fdc5a1249390d9283eb8d3ba7703ad52e3ab57
xiangrenya <xiangrenya@gmail.com>
2018-07-01 10:57:48 +0800

    去除与开发api无关的代码

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const sassMiddleware = require('node-sass-middleware');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const indexRouter = require('./routes/index.route');
 const config = require('./config/');
+
 // 连接mongodb数据库
 mongoose.connect(config.mongodb.uri);
 mongoose.connection.on('error', () => {
@@ -16,39 +15,23 @@ mongoose.connection.on('error', () => {
 
 const app = express();
 
-// 配置视图引擎
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 // 启用跨域访问
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true,
-}));
-app.use(express.static(path.join(__dirname, 'public')));
 
+// 路由管理
 app.use('/api', indexRouter);
 
-// catch 404 and forward to error handler
+// 404处理
 app.use((req, res, next) => {
   next(createError(404));
 });
 
 // 异常处理
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // res.status(err.status || 500);
-  // render the error page
-  // res.render('error');
-
   res.status(err.status || 500).json(err.message);
 });
 
